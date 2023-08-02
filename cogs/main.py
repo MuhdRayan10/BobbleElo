eb666594c15aa1b2588d33c2da58ad2622c2691e
Raj Dave <10864@bpsdoha.edu.qa>
2023-08-02 11:18:09 +0300

    main.py improvements

--- cogs/main.py
+++ cogs/main.py
@@ -2,42 +2,34 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from backend import log, embed_template, error_template, gen_game_id, InitializeGame, match_result
-import aiosqlite
+import sqlite3
 
 
 class Main(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.client.tree.sync()
+
+
     @app_commands.command(name="create-game")
     async def create_game(self, interaction):
-        print("e")
 
-
+        # Create the embed
         embed = embed_template()
-        embed = discord.Embed(title="Match", description=f"Match started by <@{interaction.user.id}>")
-        embed.add_field(name="ㅤㅤTeam 1", value=" 🤖  Player 1\n 👾  Player 2")
-        embed.add_field(name="ㅤvs", value="ㅤㅤㅤㅤ")
-        embed.add_field(name="Team 2", value="Player 3  🥷\nPlayer 4  🎃")
-
-        embed.set_footer(text="Made with <3 by muhdrayan & rajdave")
-
         embed.title = "Create Game"
+        embed.description = f"Match started by <@{interaction.user.id}>"
 
-        print(interaction)
-
-
+        # Send the embed and edit it with the view
         await interaction.response.send_message(embed=embed)
         view = InitializeGame(interaction.user.id, (await interaction.original_response()))
-
-        print("a")
         await interaction.edit_original_response(embed=embed, view=view)
 
     @app_commands.command(name='add-match')
     async def add_match(self, interaction, winner:int, player1:discord.User, player2:discord.User, player3:discord.User=None, player4:discord.User=None):
-        match_result(interaction, player1, player2, player3, player4, winner)
-
-
+        await match_result(interaction, player1, player2, player3, player4, winner)
 
 
     @app_commands.command(name="leaderboard")
@@ -47,9 +39,10 @@ class Main(commands.Cog):
         elif amount < 1:
             amount = 1
 
-        db = await aiosqlite.connect("data.db")
-        cursor = await db.execute("SELECT * FROM eco ORDER BY rating DESC LIMIT ?", (amount,))
-        res = await cursor.fetchall()
+        db = sqlite3.connect("data/elo.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM elo ORDER BY rating DESC LIMIT ?", (amount,))
+        res = cursor.fetchall()
         # res format - [(user_id, rating), (user_id, rating), ...]
 
         embed = embed_template()
@@ -57,7 +50,7 @@ class Main(commands.Cog):
 
         for i, (user_id, rating) in enumerate(res):
             user = await self.client.fetch_user(user_id)
-            embed.add_field(name=f"#{i+1} {user.name}", value=f"Rating: {rating}", inline=False)
+            embed.add_field(name=f"#{i+1} `{user.name}`", value=f"Rating: {rating}", inline=False)
 
         await interaction.response.send_message(embed=embed)
 
